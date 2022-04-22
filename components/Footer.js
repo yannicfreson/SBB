822d7948f426287e99c64d3c68c4a43ebdacf9a4
YannicFreson27 <yannic.freson1@gmail.com>
2022-04-22 10:50:37 +0200

    header and footer done

---
+++ components/Footer.js
@@ -0,0 +1,28 @@
+export default function Footer() {
+  return (
+    <footer className="w-full font-black text-white text-center">
+      <a
+        href="https://www.yannicfreson.be/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <span className="mr-1">Made with</span>
+        <span className="text-sky-300">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="mb-1 inline h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </span>
+        <span className="ml-1">by Yannic Fréson</span>
+      </a>
+    </footer>
+  );
+}
---
+++ components/Title.js
@@ -0,0 +1,16 @@
+export default function Title() {
+  return (
+    <p className="font-black w-full text-center text-7xl">
+      <span className="mr-4">
+        <span>BULLSH</span>
+        <span className="text-sky-300">I</span>
+        <span>T</span>
+      </span>
+      <span className="ml-4">
+        <span>B</span>
+        <span className="text-sky-300">I</span>
+        <span>NGO</span>
+      </span>
+    </p>
+  );
+}
